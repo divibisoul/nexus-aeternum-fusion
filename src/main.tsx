307011a549c3d392d6a311390009f98c4ea50ac6
divibisoul <divibi2011@gmail.com>
2026-08-25 18:05:04 -0300

    feat(mesh): start Nexus Soul Mesh bridge at application boot

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { startSoulNexusBridge } from './integration/SoulNexusBridge'
 import './index.css'
 
+startSoulNexusBridge()
 createRoot(document.getElementById("root")!).render(<App />);
