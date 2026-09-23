a4a93196d974c872cfa77a3dc1ea5bdbc7fbd629
divibisoul <divibi2011@gmail.com>
2026-09-23 06:59:03 -0300

    feat(aeternum): add M5 perception module index.ts

---
+++ src/modules/Perception/index.ts
@@ -0,0 +1,19 @@
+export {
+  PerceptionModule,
+  perceptionModule,
+  NeuralEventBus,
+  NeuralStateStore,
+  NeuralWormholeRegistry,
+  nervoVago,
+  hortaCore,
+  wormhole,
+} from "./PerceptionModule";
+
+export { PerceptionUI } from "./PerceptionUI";
+export { AudioRecordModule, audioRecordModule } from "./AudioRecordModule";
+export { MultimodalInputModule, multimodalInputModule } from "./MultimodalInputModule";
+export { DCRSMonitorModule, dcrsMonitorModule } from "./DCRSMonitorModule";
+export { DIASPerformanceModule, diasPerformanceModule } from "./DIASPerformanceModule";
+export { BNCv2MonitorModule, bncv2MonitorModule } from "./BNCv2MonitorModule";
+export { FusionProcessModule, fusionProcessModule } from "./FusionProcessModule";
+export { ProcessorDetailModule, processorDetailModule } from "./ProcessorDetailModule";
